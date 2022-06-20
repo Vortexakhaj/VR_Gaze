67052d4f92c4b2e067a3dd1f75aa910bcc1d1c08
HosuNyanPasu <vortex.k1@gmail.com>
2022-06-21 00:22:10 +0530

    Initial Commit

---
+++ Assets/Scripts/VRGaze.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VRGaze : MonoBehaviour
+{
+    private Animator anim;
+    //public GameObject door;
+    public Image imggaze;
+    public float totaltime = 2.0f;
+
+    bool gvrStatus;
+    bool gvrAnimStatus;
+    float gvrTimer;
+
+    public Camera vrCam;
+
+    //public AnimationClip dooropen;
+    //public AnimationClip doorclose;
+    Animation doorOpen;
+    Animation doorClose;
+    private bool isopened = false;
+    private RaycastHit hitinfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //anim = door.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            imggaze.fillAmount = gvrTimer / totaltime;
+            
+        }
+
+        if (Physics.Raycast(vrCam.transform.position, vrCam.transform.forward, out hitinfo))
+        {
+            if (imggaze.fillAmount == 1 && !isopened)
+            {
+                if (hitinfo.collider.gameObject.tag == "Door")
+                {
+                        anim = hitinfo.collider.gameObject.GetComponent<Animator>();
+                        DoorOpening("Open");
+                    Debug.Log("playing");
+                        isopened = true;                           
+                       
+                        
+                    
+                }
+            }
+
+            if(imggaze.fillAmount < 1 && isopened)
+            {
+                
+                DoorOpening("Close");
+                isopened = false;
+            }
+        }
+    }
+
+    public void Gvron()
+    {
+        gvrStatus = true;
+    }
+
+    public void Gvroff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+        imggaze.fillAmount = 0;        
+    }   
+    public void DoorOpening(string direction)
+    {
+        anim.SetTrigger(direction);       
+    }
+    
+}
---
+++ Assets/Scripts/VrLookWalk.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrLookWalk : MonoBehaviour
+{
+    public Transform vRCam;
+    [SerializeField] private float speed = 4.0f;
+    [SerializeField] private float toggangle = 30.0f;
+    private bool forwardmove ;
+    public CharacterController charC;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charC = GetComponentInParent<CharacterController>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vRCam.eulerAngles.x >= toggangle && vRCam.eulerAngles.x < 90.0f )
+                    forwardmove = true;        
+        else
+            forwardmove = false;
+
+        if (forwardmove)
+        {
+            Vector3 forwardm = vRCam.TransformDirection(Vector3.forward);
+            charC.SimpleMove(forwardm * speed);
+        }
+
+    }
+
+    
+}
